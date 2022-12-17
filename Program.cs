3c83ba4209d5bd35f7ae844bb683c8be067f17f2
Vladimir Shoshyn <vladimir.shoshyn@hotmail.com>
2022-12-17 23:28:02 +0300

    Version 2

--- Program.cs
+++ Program.cs
@@ -18,46 +18,48 @@ namespace ConsoleApp
         private const int WriteTimeSec = 2;
 
         private Random rnd = new Random();
-
         ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
 
-        internal void Read()
-        {
+        internal Task ReadAsync(int counter)
+        {            
+            queue.Enqueue(rnd.Next());
             Thread.Sleep(ReadTimeSec * 1000);
-            var addingData = rnd.Next();
-            System.Console.WriteLine($"Adding {addingData}; queue count is {queue.Count}");
-            queue.Enqueue(addingData);
+            System.Console.WriteLine($"Data readed and sending to save process. Itteration {counter}. Queue count: {queue.Count}. Data: [{string.Join("; ", queue.ToList<int>())}]");
+            
+            return Task.CompletedTask;
         }
 
-        internal void Write(int count)
-        {   
-            while (count > 0)
-            {                
-                if (queue.TryDequeue(out int savingItem))
-                {
-                    //Long saving data process
-                    Thread.Sleep(WriteTimeSec * 1000);
-                    System.Console.WriteLine($"wtited {savingItem}; queue count is {queue.Count}");
-                    count--;
-                }
-            }            
-        }
-
-        internal void Quest(int count)
+        internal void WriteData(object writeData)
         {
-            Task task = new Task(() => Write(count-1));
-            task.Start();
+            //Long saving data process            
+            Thread.Sleep(WriteTimeSec * 1000);
+            queue.TryDequeue(out int res1);
+            queue.TryDequeue(out int res2);
+            System.Console.WriteLine($"Data [{res1}; {res2}] has been saved.");
+        }
 
-            for (int i = 1; i < count; i++)
+        internal async void Quest(int count)
+        {
+            for (int i = 1; i <= count; i++)
             {
-                Read();                
-            }            
+                if(queue.Count >= 2)
+                {
+                    i--;
+                    continue;
+                } 
 
-            task.Wait();
+                while(queue.Count < 2)
+                {
+                    await ReadAsync(i);
+                }
+
+                Thread myThread = new Thread(WriteData);
+                myThread.Start(queue.ToList<int>());
+            }
 
             // Чтение и запись должны происходить одновременно.
             // Суммарное время должно быть примерно равно (кол-ву
             // повторов + 1) * большее из времени записи и чтения
-        }
+        }        
     }
 }
